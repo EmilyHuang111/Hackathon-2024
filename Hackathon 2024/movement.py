a7e633ec0222ac06a8747f700db6fc7ec669b3b1
yanis-fellache <122501964+yanis-fellache@users.noreply.github.com>
2024-11-08 23:14:55 -0500

    Update movement.py

--- Hackathon 2024/movement.py
+++ Hackathon 2024/movement.py
@@ -1,20 +1,25 @@
 import time
 import serial
+#import struct
 
 ser = serial.Serial('/dev/tty.usbserial-14120', 9600)
+time.sleep(2)
 
 def main():
-    time.sleep(2)
     run()
     print("Program Stopped")
 
 
 def move(command):
-    ser.write(command.encode())
+    c = ser.write(command.encode())
+    if command == "U":
+        #print(struct.unpack('b',ser.read(c)))
+        distance = ser.readline().decode('utf-8').strip()
+        print(distance)
 
 def run():
     while True:
-        command = input("Enter command (F/B/L/R/S): ")
+        command = input("Enter command (F/B/L/R/S/U/FS): ").upper()
         if command == "FS":
             move("S")
             ser.close()
@@ -24,3 +29,4 @@ def run():
 
 if __name__ == "__main__":
     main()
+
